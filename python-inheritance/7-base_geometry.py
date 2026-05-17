cf69e64a1d24268ddad769f087107f8e1bbeeb67
sampsonfoli16 <s.foli@alustudent.com>
2026-05-17 20:49:02 +0400

    Fix 7-base_geometry tests

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,22 +1,11 @@
-#!/usr/bin/python3
-"""Module that defines a BaseGeometry class."""
-
-
-class BaseGeometry:
-    """A class that defines base geometry."""
-
-    def area(self):
-        """Raise an Exception since area is not implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
+def integer_validator(self, name, value):
         """Validate that value is a positive integer.
 
         Args:
             name (str): The name of the parameter.
             value (int): The value to validate.
         """
-        if type(value) is not int:
+        if type(value) is not int or type(value) is bool:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))

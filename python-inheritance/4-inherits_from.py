1690fb9e9edaa955194801bd2d1ea3a42ceace9a
sampsonfoli16 <s.foli@alustudent.com>
2026-05-17 21:05:58 +0400

    Fix 4-inherits_from line too long

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,5 +3,5 @@
 
 
 def inherits_from(obj, a_class):
-    """Return True if obj is an instance of a class that inherited from a_class."""
+    """Return True if obj is an instance of a class that inherited."""
     return isinstance(obj, a_class) and type(obj) is not a_class

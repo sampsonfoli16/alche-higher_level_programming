b02ec8df6a0f51026dbf11a640a4407c7a363bce
sampsonfoli16 <s.foli@alustudent.com>
2026-05-28 14:29:13 +0400

    Fix task 1 drop and recreate user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Create user user_0d_1 with all privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;

465bf18082821335c55cc13153f7f2687c9508f1
sampsonfoli16 <s.foli@alustudent.com>
2026-06-01 17:28:22 +0400

    Fix 4-header.sh by removing -L so custom header is preserved

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a GET request with a header variable and displays the body
-url="$1"; [[ $url =~ ^https?:// ]] || url="http://$url"; curl -s -L -H "X-HolbertonSchool-User-Id: 98" "$url"
+url="$1"; [[ $url =~ ^https?:// ]] || url="http://$url"; curl -s -H "X-HolbertonSchool-User-Id: 98" "$url"

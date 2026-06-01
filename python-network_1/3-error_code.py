de28cf0a04a8ddfe335a0d84eddb5867099fec77
sampsonfoli16 <s.foli@alustudent.com>
2026-06-01 21:48:15 +0400

    Fix 3-error_code: add arg validation, UTF-8 decoding and URLError handling; make executable

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -10,11 +10,17 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+  if len(sys.argv) != 2:
+    print("Usage: ./3-error_code.py <URL>", file=sys.stderr)
+    sys.exit(1)
 
-    request = urllib.request.Request(url)
-    try:
-        with urllib.request.urlopen(request) as response:
-            print(response.read().decode("ascii"))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+  url = sys.argv[1]
+  request = urllib.request.Request(url)
+  try:
+    with urllib.request.urlopen(request) as response:
+      # decode using utf-8 to support common responses
+      print(response.read().decode("utf-8"))
+  except urllib.error.HTTPError as e:
+    print("Error code: {}".format(e.code))
+  except urllib.error.URLError as e:
+    print(e.reason, file=sys.stderr)

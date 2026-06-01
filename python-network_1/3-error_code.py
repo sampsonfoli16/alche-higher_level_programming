a6954bd92dc214a17922ea43871c96304c0efa3c
sampsonfoli16 <s.foli@alustudent.com>
2026-06-01 21:50:16 +0400

    Style: fix indentation in 3-error_code.py (PEP8)

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -10,17 +10,17 @@ import urllib.request
 
 
 if __name__ == "__main__":
-  if len(sys.argv) != 2:
-    print("Usage: ./3-error_code.py <URL>", file=sys.stderr)
-    sys.exit(1)
+    if len(sys.argv) != 2:
+        print("Usage: ./3-error_code.py <URL>", file=sys.stderr)
+        sys.exit(1)
 
-  url = sys.argv[1]
-  request = urllib.request.Request(url)
-  try:
-    with urllib.request.urlopen(request) as response:
-      # decode using utf-8 to support common responses
-      print(response.read().decode("utf-8"))
-  except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
-  except urllib.error.URLError as e:
-    print(e.reason, file=sys.stderr)
+    url = sys.argv[1]
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            # decode using utf-8 to support common responses
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+    except urllib.error.URLError as e:
+        print(e.reason, file=sys.stderr)

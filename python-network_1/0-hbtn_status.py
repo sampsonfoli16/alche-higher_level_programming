b0b663ac9fcbe4996ea1897f2a6830f6d36113ea
sampsonfoli16 <s.foli@alustudent.com>
2026-06-01 18:10:02 +0400

    Update 0-hbtn_status.py to use intranet.hbtn.io and fallback host

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 """Fetches the status from the Holberton intranet API."""
 
-from urllib import request
+from urllib import error, request
 
 if __name__ == "__main__":
-    url = 'https://alu-intranet.hbtn.io/status'
-    with request.urlopen(url) as response:
-        content = response.read()
-        print('Body response:')
-        print('\t- type:')
-        print('\t- content: {}'.format(content))
-        print('\t- utf8 content: {}'.format(content.decode('utf-8')))
+    urls = [
+        'https://intranet.hbtn.io/status',
+        'https://alu-intranet.hbtn.io/status'
+    ]
+    for url in urls:
+        try:
+            with request.urlopen(url) as response:
+                content = response.read()
+                print('Body response:')
+                print('\t- type:')
+                print('\t- content: {}'.format(content))
+                print('\t- utf8 content: {}'.format(content.decode('utf-8')))
+                break
+        except error.URLError:
+            continue

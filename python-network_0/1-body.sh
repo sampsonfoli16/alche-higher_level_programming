2aeae13f85d557d47317595135f9951fa744b696
sampsonfoli16 <s.foli@alustudent.com>
2026-06-01 17:19:35 +0400

    Fix 1-body.sh to follow redirects with curl -L

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a GET request and displays the body of a 200 status code response
-url="$1"; [[ $url =~ ^https?:// ]] || url="http://$url"; [ "$(curl -s -o /dev/null -w '%{http_code}' "$url")" = "200" ] && curl -s "$url"
+url="$1"; [[ $url =~ ^https?:// ]] || url="http://$url"; [ "$(curl -s -L -o /dev/null -w '%{http_code}' "$url")" = "200" ] && curl -s -L "$url"

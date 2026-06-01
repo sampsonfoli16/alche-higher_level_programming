0936216b94029c7e0b312c7d50e380cde9e157ee
sampsonfoli16 <s.foli@alustudent.com>
2026-06-01 21:27:45 +0400

    file 0 edited

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status using urllib"""
+"""Script that fetches https://alu-intranet.hbtn.io/status using urllib"""
 import urllib.request
 
-
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as r:
-        body = r.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as r:
+    body = r.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))

cc2f7b728d341b0d25bbb3a4a4b0bf7bfa6b761e
sampsonfoli16 <s.foli@alustudent.com>
2026-06-01 18:08:02 +0400

    Fix 0-hbtn_status.py output format for type line

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with request.urlopen(url) as response:
         content = response.read()
         print('Body response:')
-        print('\t- type: {}'.format(type(content)))
+        print('\t- type:')
         print('\t- content: {}'.format(content))
         print('\t- utf8 content: {}'.format(content.decode('utf-8')))

56b583eee46ed9dcfdf6e413001ed128031e923d
sampsonfoli16 <s.foli@alustudent.com>
2026-06-01 21:32:53 +0400

    Fix docstring and PEP8 in file 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Script that fetches a URL and displays the body response."""
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
-with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as r:
-    body = r.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))

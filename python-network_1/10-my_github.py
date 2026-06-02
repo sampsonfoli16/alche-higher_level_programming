21b84a415fe116e49a0d8eb645dea0c13e76810b
sampsonfoli16 <s.foli@alustudent.com>
2026-06-02 15:43:12 +0400

    Fix 10-my_github.py: wrap long request line, add arg validation, make executable

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -5,9 +5,15 @@ import requests
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./10-my_github.py <username> <password>", file=sys.stderr)
+        sys.exit(1)
+
     username = sys.argv[1]
     password = sys.argv[2]
-    response = requests.get('https://api.github.com/user', auth=(username, password))
+    response = requests.get(
+        'https://api.github.com/user', auth=(username, password)
+    )
     if response.ok:
         print(response.json().get('id'))
     else:

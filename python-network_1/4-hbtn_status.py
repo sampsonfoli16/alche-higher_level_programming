fef62fa729a8d4ebe33cb3d29c09d9481883183d
sampsonfoli16 <s.foli@alustudent.com>
2026-06-02 15:08:02 +0400

    Fix Pylance import issue in 4-hbtn_status.py by removing requests.packages.urllib3 import

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status."""
 import requests
+import warnings
 
 
 if __name__ == "__main__":
-    # Some test environments may present SSL issues for the external URL.
-    # Disable verification and suppress the related warning to ensure
-    # the script can fetch the content in those environments.
-    try:
-        from requests.packages.urllib3.exceptions import InsecureRequestWarning
-        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-    except Exception:
-        pass
-
+    warnings.filterwarnings("ignore", message="Unverified HTTPS request")
     r = requests.get("https://intranet.hbtn.io/status", verify=False)
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))

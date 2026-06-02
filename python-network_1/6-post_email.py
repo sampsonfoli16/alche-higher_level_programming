c9be8c493d16138e3deef6d2142aba7afd9a5c83
sampsonfoli16 <s.foli@alustudent.com>
2026-06-02 15:16:44 +0400

    Fix 6-post_email.py: add arg validation and make executable

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -4,7 +4,12 @@
 import requests
 import sys
 
+
 if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./6-post_email.py <URL> <email>", file=sys.stderr)
+        sys.exit(1)
+
     url = sys.argv[1]
     email = sys.argv[2]
     response = requests.post(url, data={'email': email})

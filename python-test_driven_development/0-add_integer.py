03493fb7275bddc50e3f3b60bc2e25e0ad18254b
sampsonfoli16 <s.foli@alustudent.com>
2026-06-06 06:56:16 +0000

    Fix 0-add_integer PEP8 and docstrings

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
-""" Doc """
+"""This module defines a function that adds two integers."""
+
 
 def add_integer(a, b=98):
-    """ Doc """
+    """Add two integers and return the result.
+
+    Args:
+        a (int/float): The first number.
+        b (int/float): The second number, default is 98.
+    Raises:
+        TypeError: If a or b is not an integer or float.
+    Returns:
+        The integer addition of a and b.
+    """
     if a != a:
         a = 89
     if b != b:
